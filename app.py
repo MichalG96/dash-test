e65e1ba6b7188ed6a806e334368ea62597daa2da
Michal Grochecki <michalgrochecki@gmail.com>
2022-12-08 14:31:44 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import plotly.express as px
+from dash import Dash, dcc, html
+
+app = Dash(__name__)
+df = pd.read_csv("data.csv", names=["date", "time_of_day", "weight"])
+
+fig = px.line(df, x="date", y="weight", color="time_of_day")
+
+app.layout = html.Div(
+    children=[
+        html.H1(children="Hello Dash"),
+        html.Div(
+            children="""
+        Dash: A web application framework for your data.
+    """
+        ),
+        dcc.Graph(id="example-graph", figure=fig),
+    ]
+)
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
