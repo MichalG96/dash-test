501cffaefac8cf88c36560422ad3e09b964cfd8e
Michal Grochecki <michalgrochecki@gmail.com>
2022-12-13 10:52:08 +0100

    Prettify plot

--- app.py
+++ app.py
@@ -10,7 +10,19 @@ df = pd.read_csv("data.csv", names=["date", "time_of_day", "weight"]).replace(
     {"r": "morning", "w": "evening"}
 )
 
-fig = px.line(df, x="date", y="weight", color="time_of_day", line_shape="spline")
+# TODO: calculate the average for each day, plot moving average for the last 4/7 days
+
+fig = px.line(
+    df,
+    x="date",
+    y="weight",
+    color="time_of_day",
+    line_shape="spline",
+    markers=True,
+    text="weight",
+)
+fig.update_traces(textposition="bottom right")
+
 
 app.layout = html.Div(
     children=[
