8a84985b8059aafdfb25184aaca7e6580a805cf6
Michal Grochecki <michalgrochecki@gmail.com>
2022-12-14 10:14:39 +0100

    Mark diet start

--- app.py
+++ app.py
@@ -6,9 +6,7 @@ app = Dash(__name__)
 
 server = app.server
 
-df = pd.read_csv("data.csv").replace(
-    {"r": "morning", "w": "evening"}
-).dropna()
+df = pd.read_csv("data.csv").replace({"r": "morning", "w": "evening"}).dropna()
 
 # TODO: calculate the average for each day, plot moving average for the last 4/7 days
 
@@ -22,7 +20,13 @@ fig = px.line(
     text="weight",
 )
 fig.update_traces(textposition="bottom right")
-
+fig.add_vline(
+    x="2022-12-13",
+    line_width=1,
+    line_dash="dash",
+    line_color="green",
+    # annotation="Diet start",
+)
 
 app.layout = html.Div(
     children=[
