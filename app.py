194898a7f1f77b944922981ed408da6ba9e5ba2c
Michal Grochecki <michalgrochecki@gmail.com>
2022-12-18 22:29:17 +0100

    App improvements

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import datetime
+import random
 
 import numpy as np
 import pandas as pd
@@ -42,7 +43,7 @@ def get_trendline(df: pd.DataFrame, weekly_coefficient: float = 0.5) -> tuple:
     x = pd.date_range(start=x_start, end=x_end)
     daily_coefficient = weekly_coefficient / 7
     end_avg_weight = start_avg_weight - (daily_coefficient * len(x))
-    y = np.squeeze(np.linspace(start_avg_weight, end_avg_weight, len(x)))
+    y = np.round(np.squeeze(np.linspace(start_avg_weight, end_avg_weight, len(x))), 2)
     return x, y
 
 
@@ -65,31 +66,76 @@ def get_avg_df(original_df: pd.DataFrame) -> pd.DataFrame:
     return original_df
 
 
-def moving_average(arr, window_size: int = 7):
-    result = np.convolve(arr, np.ones(window_size), "valid") / window_size
-    result = np.pad(
-        result, (len(arr) - len(result), 0), "constant", constant_values=(np.nan,)
-    )
-    return result
+# Alternative way for moving average.
+# Later discovered pandas' df.rolling(window_size).mean()
+# def moving_average(arr, window_size: int = 7):
+#     result = np.convolve(arr, np.ones(window_size), "valid") / window_size
+#     result = np.pad(
+#         result, (len(arr) - len(result), 0), "constant", constant_values=(np.nan,)
+#     )
+#     return result
+
+
+def add_weight_text(df, fig):
+    col_name_y_mapping = {
+        "weight_morning": -15,
+        "weight_evening": 15,
+        "avg_weight": -10,
+    }
+
+    def add_text_for_column(col_name):
+        if not np.isnan(getattr(row, col_name)):
+            fig.add_annotation(
+                x=row.Index,
+                y=getattr(row, col_name),
+                text=f"{getattr(row, col_name)}",
+                showarrow=False,
+                yshift=col_name_y_mapping[col_name],
+                xshift=10,
+            )
+
+    for row in df.itertuples():
+        for col in col_name_y_mapping:
+            add_text_for_column(col)
 
 
 df = prepare_data()
 df = get_avg_df(df)
 trendline_x, trendline_y = get_trendline(df)
-df["moving_average"] = moving_average(df["avg_weight"])
-
+# df["moving_average"] = moving_average(df["avg_weight"])
+df["moving_average"] = df["avg_weight"].rolling(7).mean()
+df = df.round(2)
+# print(df)
 # TODO: add legend, labels and axis names
 
 fig = px.line(
     df,
     markers=True,
     line_shape="spline",
+    height=700,  # in px
+    # For some reason makes the trace disappear
+    # hover_data={'weight_evening': ':.2f'}
 )
-fig.update_traces(connectgaps=True)
+fig.update_traces(connectgaps=True, textposition="bottom right", marker_size=6)
+
+# TODO:
+#   Adds text for each trace. Problem: text doesn't disappear when trace is hidden
+# add_weight_text(df, fig)
+def rand_func():
+    if random.random() > 0.6:
+        return "green"
+    return "red"
+
 
 fig.add_trace(
     go.Scatter(
-        x=trendline_x, y=trendline_y, mode="lines+markers", name="Expected loss rate"
+        x=trendline_x,
+        y=trendline_y,
+        mode="lines",
+        name="Expected loss rate",
+        fill="tonexty",
+        fillcolor="rgba(0,0,0, 0.2)"
+        # fillcolor=rand_func()
     )
 )
 
@@ -106,14 +152,13 @@ fig.update_layout(
         df.index[0] - datetime.timedelta(days=1),
         df.index[-1] + datetime.timedelta(days=1),
     ],
-    yaxis_range=[min(df["weight_morning"] - 1), max(df["weight_evening"]) + 1],
+    yaxis_range=[min(df["weight_morning"] - 0.5), max(df["weight_evening"]) + 0.5],
 )
 app.layout = html.Div(
     children=[
-        html.H1(children="Welcome"),
         dcc.Graph(id="example-graph", figure=fig),
     ]
 )
 
 if __name__ == "__main__":
-    app.run_server(debug=False)
+    app.run_server(debug=True)
